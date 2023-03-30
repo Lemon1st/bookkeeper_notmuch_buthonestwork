a39d942193994a23663d5d3f448e4b7fbe7781aa
Lemonist <sasha91234@mail.ru>
2023-03-30 10:31:25 +0300

    Добавлены тесты репозитория

--- tests/test_repository/test_sqlite_repository.py
+++ tests/test_repository/test_sqlite_repository.py
@@ -34,3 +34,42 @@ def test_crud(repository):
     assert repository.get(pk1) == obj2
     repository.delete(pk2)
     assert repository.get(pk2) is None
+
+
+def test_get_all(repository):
+    data_size = 5
+    repository.delete_all()
+    objects = [Custom(pk=i + 1) for i in range(data_size)]
+    for o in objects:
+        repository.add(o)
+    data = repository.get_all()
+    for i in range(data_size):
+        assert data[i] == objects[i]
+
+
+def test_get_all_where(repository):
+    data_size = 5
+    repository.delete_all()
+    objects = []
+    for i in range(data_size):
+        o = Custom(pk=i + 1)
+        o.name = str(i)
+        o.test = 'testing'
+        repository.add(o)
+        objects.append(o)
+    assert repository.get_all({'name': '0'})[0] == objects[0]
+    data = repository.get_all({'testing': 'testing'})
+    for i in range(data_size):
+        assert data[i] == objects[i]
+
+
+def test_cant_update(repository):
+    obj = Custom()
+    print(obj)
+    with pytest.raises(TypeError):
+        repository.update(obj)
+
+
+def test_cannot_delete_nonexistent_file(repository):
+    with pytest.raises(KeyError):
+        repository.delete(100)
